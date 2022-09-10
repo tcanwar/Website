ffbeb5bab5585c66eedf5b81a671033c423c7588
[rawnact] <tcanwar14@gmail.com>
2022-09-10 14:02:18 -0400

    20220910_v2

--- country_map.js
+++ country_map.js
@@ -1,6 +1,6 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 2);
 L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> ©tcanwar'
+    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> ©tcanwar'
     }).addTo(mymap);
 
 var myStyle = {
